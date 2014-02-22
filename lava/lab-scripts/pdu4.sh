8cfb569b1de021ff83a6b86aa68d1c570c0272f8
Tyler Baker <tyler.baker@linaro.org>
2014-02-22 14:23:12 -0800

    add pdu4 script

---
+++ lava/lab-scripts/pdu4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Example ./pdu4.sh <ip_addr> <port> <0/1>
+if [ $2 -gt 0 ] && [ $2 -lt 9 ]
+then
+        if [ "$#" -eq 3 ]
+        then
+		echo "BANK 1: Controlling Port: $2"
+		snmpset -v1 -c public $1 1.3.6.1.4.1.92.58.2.$2.0 i $3
+        elif [ "$#" -eq 2 ]
+        then
+		echo "BANK 1: Resetting Port: $2"
+		snmpset -v1 -c public $1 1.3.6.1.4.1.92.58.2.$2.0 i 0
+                sleep 10
+	        snmpset -v1 -c public $1 1.3.6.1.4.1.92.58.2.$2.0 i 1
+        else
+		echo "Too many arguments"
+        fi
+else
+	echo "Port number is out of range"
+fi
