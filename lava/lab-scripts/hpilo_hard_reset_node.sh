c034253affe5c6916f549d13b9a3ae47bc10ad97
Tyler Baker <tyler.baker@linaro.org>
2014-05-10 16:38:52 -0700

    hpilo: tweak hard reset script

--- lava/lab-scripts/hpilo_hard_reset_node.sh
+++ lava/lab-scripts/hpilo_hard_reset_node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./hpilo_power_off_node.exp $1
+/usr/local/lab-scripts/hpilo_power_off_node.exp $1
 sleep 20
-./hpilo_power_on_node.exp $1
+/usr/local/lab-scripts/hpilo_power_on_node.exp $1
